fe5d9cf913e7d3af078d90267104383eef605d53
CR7MentalityN1 <dolphi034@gmail.com>
2026-05-14 02:38:00 +0300

    Initial commit: project structure and README

--- src/LibraryApp/Models/Library.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace LibraryApp.Models {
-    public class Library {
-        public List<Book> Books = new List<Book>();
-        public void AddBook(Book book) => Books.Add(book);
-    }
-}
